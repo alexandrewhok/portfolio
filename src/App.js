f6ee98c62fa3776923f2185a7ddd21e085086c7d
Alexandre Lucas <alexandrelucas@Alexandres-MacBook-Pro.local>
2022-01-03 18:01:55 +0000

    first commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import {BrowserRouter as Router,Route, Switch} from "react-router-dom";
+import Home from './Pages/home';
+import About from './Pages/about';
+import Contact from './Pages/contact';
+import './Styles/applications.scss';
+
+
+
+
+function App() {
+  return ( 
+   <Router>
+     <Switch>
+       <Route path='/' component={Home} exact />
+       <Route path='/about' component={About} exact />
+       <Route path='/contact' component={Contact} exact />
+     </Switch>
+   </Router>
+  );
+}
+
+export default App;
